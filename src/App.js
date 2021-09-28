2368f7561e0e203d10e415ecdb723adeddc954da
Joyanta Mondal <56169323+w3codelabs@users.noreply.github.com>
2021-09-28 19:38:14 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Newspaper from './components/Newspaper/Newspaper';
+
+function App() {
+  return (
+    <div className="App">
+      <Newspaper></Newspaper>
+    </div>
+  );
+}
+
+export default App;
